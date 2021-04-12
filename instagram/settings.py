80fa93dbb37ee3c746cf772547b8bfd7a48e2808
angels101 <angelscodex101@gmail.com>
2021-04-12 15:28:12 +0300

    urls updates

--- instagram/settings.py
+++ instagram/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'psycopg2',
     'posts',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+##
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
