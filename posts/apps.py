40f9c1514152f15e22928e267dbb001025b9466e
angels101 <angelscodex101@gmail.com>
2021-04-12 12:05:41 +0300

    modified apps&admin

--- posts/apps.py
+++ posts/apps.py
@@ -1,6 +1,8 @@
+"""Posts application module"""
 from django.apps import AppConfig
 
 
 class PostsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'posts'
+    verbose_name = 'Posts'
